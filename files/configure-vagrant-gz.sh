13399ca9f80294bca759d297254730fe3151415c
James Nugent <james@jen20.com>
2017-05-31 14:34:45 +0100

    Add svc:/network/physical as a dependent

--- files/configure-vagrant-gz.sh
+++ files/configure-vagrant-gz.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+set -o errexit
+set -o pipefail
+set -o xtrace
+
 ADMIN_MAC=$(dladm show-phys -m -p -o address | head -n 1)
 sed -i '' "s/admin_nic=.*/admin_nic=$ADMIN_MAC/" /usbkey/config
 sysinfo -u
-
-svcadm disable -s svc:/network/physical:default
-svcadm enable -s svc:/network/physical:default
